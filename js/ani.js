4770a950eb90db7faceb88259698a0cc79b9258a
e24p21 <e24p21@gmail.com>
2015-10-06 22:39:16 +0800

    debug

--- js/ani.js
+++ js/ani.js
@@ -1,7 +1,6 @@
 // for login page animation
 document.getElementById('toggleProfile').addEventListener('click', function () {
     document.location.href="https://instagram.com/oauth/authorize/?client_id=dc00b387fa124add80a40a600e1d2e01&redirect_uri=http://localhost/instagram/success.html&response_type=token";
-  });
 });
 
 
