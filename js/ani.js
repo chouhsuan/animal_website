6e49a03dfc409e24e9f1a39f8b94d86107e8269b
e24p21 <e24p21@gmail.com>
2015-10-06 22:32:38 +0800

    reload to instagram login page

--- js/ani.js
+++ js/ani.js
@@ -1,10 +1,7 @@
 // for login page animation
 document.getElementById('toggleProfile').addEventListener('click', function () {
-  [].map.call(document.querySelectorAll('.profile'), function(el) {
-    el.classList.toggle('profile--open');
+    document.location.href="https://instagram.com/oauth/authorize/?client_id=dc00b387fa124add80a40a600e1d2e01&redirect_uri=http://localhost/instagram/success.html&response_type=token";
   });
 });
 
 
-
-//
